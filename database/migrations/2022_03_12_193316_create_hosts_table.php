54d3e6d40c6b35c90ddc620d6d56007058e71f3d
MojtabaSafarkhani <mojtabasfr01@gmail.com>
2022-03-12 23:14:15 +0330

    create model and migration of host

---
+++ database/migrations/2022_03_12_193316_create_hosts_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateHostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('hosts', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->unique()->constrained();
+            $table->string('phone', 25);
+            $table->text('address');
+            $table->string('national_code', 10)->unique();
+            $table->string('national_card_photo');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('hosts');
+    }
+}
