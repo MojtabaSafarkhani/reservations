2dbd485addac6425213f8747d0534ffea334538f
MojtabaSafarkhani <mojtabasfr01@gmail.com>
2022-04-02 17:40:37 +0430

    show gallery in slider

--- resources/views/client/hotels/show.blade.php
+++ resources/views/client/hotels/show.blade.php
@@ -11,46 +11,52 @@
     </div>
     <div class="my-5"></div>
     <div class="container">
-        <div class="row">
-            <div class="col-md-7  mx-auto ">
-                <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
-                    <div class="carousel-indicators">
-                        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0"
-                                class="active" aria-current="true" aria-label="Slide 1"></button>
-                        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1"
-                                aria-label="Slide 2"></button>
-                        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2"
-                                aria-label="Slide 3"></button>
+        @if($hotel->galleries()->count()>0)
+
+            @php
+                $images=$hotel->galleries;
+
+            $active=random_int(0,($hotel->galleries()->count()-1));
+            @endphp
+            <div class="row">
+                <div class="col-md-7  mx-auto ">
+                    <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
+                        <div class="carousel-indicators">
+                            @foreach($images as $key=>$image)
+                                <button type="button" data-bs-target="#carouselExampleIndicators"
+                                        data-bs-slide-to="{{$key}}"
+                                        class="active" aria-current="true" aria-label="Slide 1"></button>
+                            @endforeach
+                        </div>
+                        <div class="carousel-inner">
+
+                            @foreach($images as $key=>$image)
+
+                                <div class="carousel-item
+                                              @if($key==$active)
+                                    active
+                                @endif
+                                    ">
+                                    <img src="{{$image->image_url}}"
+                                         class="d-block w-100"
+                                         alt="...">
+                                </div>
+                            @endforeach
+                        </div>
+                        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators"
+                                data-bs-slide="prev">
+                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                            <span class="visually-hidden">Previous</span>
+                        </button>
+                        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators"
+                                data-bs-slide="next">
+                            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                            <span class="visually-hidden">Next</span>
+                        </button>
                     </div>
-                    <div class="carousel-inner">
-                        <div class="carousel-item active">
-                            <img src="https://media.tehrantimes.com/d/t/2021/06/20/4/3808608.jpg" class="d-block w-100"
-                                 alt="...">
-                        </div>
-                        <div class="carousel-item">
-                            <img
-                                src="https://www.potatobusiness.com/cwsd.php?Z3AuX2txZ3FhKUNzfW5_LENRI2l_aQ.jpg"
-                                class="d-block w-100" alt="...">
-                        </div>
-                        <div class="carousel-item">
-                            <img
-                                src="https://www.europeanregionofgastronomy.org/wp-content/uploads/2016/05/3020337-poster-p-1-with-farmplicity-the-farm-to-table-movement-meets-the-21st-century.jpg"
-                                class="d-block w-100" alt="...">
-                        </div>
-                    </div>
-                    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators"
-                            data-bs-slide="prev">
-                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                        <span class="visually-hidden">Previous</span>
-                    </button>
-                    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators"
-                            data-bs-slide="next">
-                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                        <span class="visually-hidden">Next</span>
-                    </button>
                 </div>
             </div>
-        </div>
+        @endif
         <div class="my-3"></div>
         <div class="row">
             <div class="col-md-7  mx-auto">
